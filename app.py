92b5bb1f03b7103594e9ceb33d901401229fc7c8
tyfvf <tyfvf2@gmail.com>
2022-12-23 19:03:43 -0300

    comecei a usar uma nova lib chamada customtkinter pq deixa os widgets mais bonitos

---
+++ app.py
@@ -0,0 +1,48 @@
+from customtkinter import *
+from tkinter import *
+from tkinter import ttk
+from menu import Menu
+
+class App(Menu):
+    def __init__(self):
+        set_default_color_theme('dark-blue')
+        set_appearance_mode('dark')
+        self.root = CTk()
+        self.tela()
+        self.menu()
+        self.root.mainloop()
+        
+
+    def tela(self):
+        self.root.geometry('1000x700+450+150')
+        self.root.title('Acervo Biblioteca')
+        self.root.resizable(False, False)
+
+
+    def menu(self):
+        self.lb_menu = CTkLabel(self.root, text='Bem-vindo ao acervo da Biblioteca', font=('Times new Roman', 36, 'bold'))
+        self.lb_menu.place(relx=0.25, rely=0.05)
+
+        self.bt_cadastro_usuario = CTkButton(self.root, text='Cadastrar usuário', font=('Arial', 14), width=250, command=self.cadastrar_usuario)
+        self.bt_cadastro_usuario.place(relx=0.39, rely=0.2)
+
+        self.bt_listar_usuario = CTkButton(self.root, text='Listar usuários', font=('Arial', 14), width=250)
+        self.bt_listar_usuario.place(relx=0.39, rely=0.3)
+
+        self.bt_cadastro_livro = CTkButton(self.root, text='Cadastrar livro', font=('Arial', 14), width=250)
+        self.bt_cadastro_livro.place(relx=0.39, rely=0.4)
+
+        self.bt_listar_livro = CTkButton(self.root, text='Listar livros', font=('Arial', 14), width=250)
+        self.bt_listar_livro.place(relx=0.39, rely=0.5)
+
+        self.bt_pegar_livro = CTkButton(self.root, text='Pegar livro', font=('Arial', 14), width=250)
+        self.bt_pegar_livro.place(relx=0.39, rely=0.6)
+
+        self.bt_registro_emprestimo = CTkButton(self.root, text='Registro Empréstimos', font=('Arial', 14), width=250)
+        self.bt_registro_emprestimo.place(relx=0.39, rely=0.7)
+
+        self.bt_devolver_livro = CTkButton(self.root, text='Devolver livro', font=('Arial', 14), width=250)
+        self.bt_devolver_livro.place(relx=0.39, rely=0.8)
+
+
+App()
--- menu.py
+++ menu.py
@@ -1,69 +1,84 @@
+from customtkinter import *
+from tkinter import *
+from tkinter import ttk
 from datetime import datetime
 from usuario import Usuario
 from livro import Livro
 from emprestimo import Emprestimo
 
-def cadastrar_usuarios(usuarios):
-    nome = input('Qual o nome do usuário? ')
-    usuarios.append(Usuario(len(usuarios), nome))
-    print('Usuário cadastrado!')
+class Menu():
+
+    def cadastrar_usuario(self):
+        self.cadastrar_usuario_top_level = CTkToplevel(self.root)
+        self.cadastrar_usuario_top_level.title('Cadastrar usuário')
+        self.cadastrar_usuario_top_level.resizable(False, False)
+        self.cadastrar_usuario_top_level.focus_force()
+
+        self.lb_nome_usuario = CTkLabel(self.cadastrar_usuario_top_level, text='Nome', font=('Arial', 16))
+        self.lb_nome_usuario.place(relx=0.4, rely=0.1)
+
+        self.entry_nome_usuario = CTkEntry(self.cadastrar_usuario_top_level, font=('Arial', 12))
+        self.entry_nome_usuario.place(relx=0.15, rely=0.25)
+
+        self.bt_cadastrar_usuario = CTkButton(self.cadastrar_usuario_top_level, text='Cadastrar', font=('Arial', 12))
+        self.bt_cadastrar_usuario.place(relx=0.15, rely=0.45)
 
 
-def listar_usuarios(usuarios):
-    if len(usuarios) == 0:
-        print('Nenhum usuário cadastrado ainda')
-    else:
-        for u in usuarios:
-            print(u)
-
-
-def cadastrar_livros(livros):
-    nome = input('Qual o nome do livro? ')
-    livros.append(Livro(len(livros), nome))
-    print('Livro cadastrado com sucesso!')
-
-
-def listar_livros(livros):
-    if len(livros) == 0:
-        print('Nenhum livro cadastrado ainda')
-    else:
-        for l in livros:
-            print(l)
-
-
-def pegar_livro(usuarios, livros, emprestimos):
-    id = int(input('Qual o id do usuário que irá pegar? '))
-    if id < 0 or id >= len(usuarios):
-        print('Esse ID não existe')
-    else:
-        id_livro = int(input(f'Bem-vindo {usuarios[id].nome}, qual o id do livro que você quer pegar? '))
-        if id_livro < 0 or id_livro >= len(livros):
-            print('Esse ID não existe')
-        elif livros[id_livro].emprestado:
-            print('Esse livro foi emprestado, por favor aguarde.')
+    def listar_usuarios(usuarios):
+        if len(usuarios) == 0:
+            print('Nenhum usuário cadastrado ainda')
         else:
-            emprestimos.append(Emprestimo(len(emprestimos), usuarios[id], livros[id_livro], datetime.now()))
-            livros[id_livro].emprestado = True
-            print(f'{usuarios[id].nome} pegou {livros[id_livro].nome} emprestado!')
+            for u in usuarios:
+                print(u)
 
 
-def registro_emprestimos(emprestimos):
-    if len(emprestimos) == 0:
-        print('Nenhum empréstimo feito')
-    else:
-        for e in emprestimos:
-            print(e)
+    def cadastrar_livros(livros):
+        nome = input('Qual o nome do livro? ')
+        livros.append(Livro(len(livros), nome))
+        print('Livro cadastrado com sucesso!')
 
 
-def devolver_livro(emprestimos):
-    id_emprestimo = int(input('Qual o id do empréstimo? '))
-    if id_emprestimo < 0 or id_emprestimo >= len(emprestimos):
-        print('Esse ID não existe')
-    elif emprestimos[id_emprestimo].devolvido:
-        print('Esse empréstimo já está quitado')
-    else:
-        emprestimos[id_emprestimo].livro.emprestado = False
-        emprestimos[id_emprestimo].devolvido = True
-        emprestimos[id_emprestimo].data_entrega = datetime.now()
-        print('Livro devolvido com sucesso!')
-        
+    def listar_livros(livros):
+        if len(livros) == 0:
+            print('Nenhum livro cadastrado ainda')
+        else:
+            for l in livros:
+                print(l)
+
+
+    def pegar_livro(usuarios, livros, emprestimos):
+        id = int(input('Qual o id do usuário que irá pegar? '))
+        if id < 0 or id >= len(usuarios):
+            print('Esse ID não existe')
+        else:
+            id_livro = int(input(f'Bem-vindo {usuarios[id].nome}, qual o id do livro que você quer pegar? '))
+            if id_livro < 0 or id_livro >= len(livros):
+                print('Esse ID não existe')
+            elif livros[id_livro].emprestado:
+                print('Esse livro foi emprestado, por favor aguarde.')
+            else:
+                emprestimos.append(Emprestimo(len(emprestimos), usuarios[id], livros[id_livro], datetime.now()))
+                livros[id_livro].emprestado = True
+                print(f'{usuarios[id].nome} pegou {livros[id_livro].nome} emprestado!')
+
+
+    def registro_emprestimos(emprestimos):
+        if len(emprestimos) == 0:
+            print('Nenhum empréstimo feito')
+        else:
+            for e in emprestimos:
+                print(e)
+
+
+    def devolver_livro(emprestimos):
+        id_emprestimo = int(input('Qual o id do empréstimo? '))
+        if id_emprestimo < 0 or id_emprestimo >= len(emprestimos):
+            print('Esse ID não existe')
+        elif emprestimos[id_emprestimo].devolvido:
+            print('Esse empréstimo já está quitado')
+        else:
+            emprestimos[id_emprestimo].livro.emprestado = False
+            emprestimos[id_emprestimo].devolvido = True
+            emprestimos[id_emprestimo].data_entrega = datetime.now()
+            print('Livro devolvido com sucesso!')
+            
